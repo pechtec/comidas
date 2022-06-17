5835313d9b0e90013a405bb18919104ade33eec6
pechtec <107696217+pechtec@users.noreply.github.com>
2022-06-17 11:35:24 -0500

    primero
    primero

---
+++ guardar.php
@@ -0,0 +1,29 @@
+<?php
+ //conectamos Con el servidor
+ $host ="localhost";
+ $user ="root";
+ $pass ="";
+ $db="pedidos";
+
+ //funcion llamada conexion con (dominio,usuarios,contraseña,base_de_datos)
+ $con = mysqli_connect($host,$user,$pass,$db)or die("Problemas al Conectar");
+ mysqli_select_db($con,$db)or die("problemas al conectar con la base de datos");
+
+
+ //recuperar las variables
+ $nombre=$_POST['nombre'];
+$Direccion=$_POST['direccion'];
+$Pedidos=$_POST['pedidos'];
+ //hacemos la sentencia de sql
+ $sql="INSERT INTO datos VALUES('$nombre',
+           '$Direccion',
+           '$Pedidos')";
+ //ejecutamos la sentencia de sql
+ $ejecutar=mysqli_query($con,$sql);
+ //verificamos la ejecucion
+ if(!$ejecutar){
+  echo"Hubo Algun Error";
+ }else{
+  echo"Datos Guardados Correctamente<br><a href='index.html'>Volver</a>";
+ }
+?>
